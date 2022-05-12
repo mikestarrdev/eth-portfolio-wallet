3c891ea63195390c56f90ee6d6b29ccb0ce9b160
Mike Starr <imhs@protonmail.com>
2022-05-12 15:07:45 -0700

    Build component and Ethers logic to generate new wallet + mnemonic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { useState } from "react";
+import Wallet from "./components/Wallet";
+
 function App() {
+  const [userWallet, setUserWallet] = useState();
+
   return (
     <>
-      <header>Wealth Walllet 1.0</header>
+      <header>
+        <h1>Welcome To Your Crypto Dashboard</h1>
+      </header>
+      <Wallet userWallet={userWallet} setUserWallet={setUserWallet} />
     </>
   );
 }
---
+++ src/components/Wallet.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import { ethers } from "ethers";
+require("dotenv").config();
+
+// const INFURA_ID = process.env.INFURA_ID;
+// const provider = new ethers.providers.JsonRpcProvider(`https://rinkeby.infuraiov3/${INFURA_ID}`);
+
+function Wallet({ userWallet, setUserWallet }) {
+  const [walletCreated, setWalletCreated] = useState(false);
+
+  function createWallet() {
+    const wallet = new ethers.Wallet.createRandom();
+    setWalletCreated(true);
+    setUserWallet(wallet);
+  }
+
+  function confirmMnemonic() {
+    setWalletCreated(false);
+    setUserWallet();
+  }
+
+  function accessWallet() {}
+
+  return (
+    <div className="wallet">
+      {!walletCreated ? (
+        <button onClick={createWallet}>Create new wallet</button>
+      ) : null}
+      {walletCreated ? (
+        <>
+          <div>
+            <p>
+              Address: <strong>{userWallet.address}</strong>
+            </p>
+            <p>
+              <span className="warning">
+                ATTENTION: do NOT reveal this 12-word mnemonic phrase to anyone!
+                It can be used to recover your wallet, but also to steal your
+                funds! Write down these 12 words in a safe place which is NOT
+                your computer
+              </span>
+              <br />
+              <p className="mnemonic">{userWallet.mnemonic.phrase}</p>
+            </p>
+            <p>
+              Click "confirm" after you've secured your mnemonic phrase. (You
+              will NEVER be able to view it again)
+            </p>
+            <button onClick={confirmMnemonic}>Confirm</button>
+          </div>
+        </>
+      ) : null}
+
+      <h3>Balances</h3>
+    </div>
+  );
+}
+
+export default Wallet;
