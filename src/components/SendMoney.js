65333251acf03ae0b57bb3ad13bed47afbba06d9
Mike Starr <imhs@protonmail.com>
2022-05-14 15:03:03 -0700

    Added to  SendMoney form

--- src/components/SendMoney.js
+++ src/components/SendMoney.js
@@ -1,58 +1,143 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const { ethers } = require("ethers");
+require("dotenv").config();
 
 function SendMoney() {
+  const [walletType, setWalletType] = useState("encrypted-JSON");
+  const [encryptedJSON, setEncryptedJSON] = useState();
+  const [mnemonic, setMnemonic] = useState("");
+  const [addressFrom, setAddressFrom] = useState("");
   const [addressTo, setAddressTo] = useState("");
   const [units, setUnits] = useState("Ether");
-  const [amount, setAmount] = useState();
-  const [gas, setGas] = useState();
+  const [amount, setAmount] = useState(0);
+  const [gas, setGas] = useState(21000);
+  const [key, setKey] = useState("0x");
+  const [password, setPassword] = useState("");
+  const [tx, setTx] = useState({});
 
-  function signTransaction() {
-    console.log(addressTo);
-  }
+  let navigate = useNavigate();
+
+  const signTransaction = async (e) => {
+    const INFURA_ID = process.env.INFURA_ID;
+    const provider = new ethers.providers.JsonRpcProvider(
+      `https://rinkeby.infura.io/v3/${INFURA_ID}`
+    );
+    const signer = provider.getSigner();
+    let wallet;
+    {
+      encryptedJSON
+        ? (wallet = new ethers.Wallet.fromEncryptedJson(
+            encryptedJSON,
+            password
+          ))
+        : (wallet = new ethers.Wallet(key, provider));
+    }
+
+    const tx = await wallet.sendTransaction({
+      to: addressTo,
+      value: ethers.utils.parseEther(amount),
+    });
+
+    await tx.wait();
+    await setTx(tx);
+    console.log(tx);
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={signTransaction}>
         <label>
-          Upload JSON keystore: <input type="file" />
+          How would you like to access your wallet?{" "}
+          <select
+            onChange={(e) => {
+              setWalletType(e.target.value);
+            }}
+          >
+            <option value="encrypted-JSON">Encrypted JSON</option>
+            <option value="mnemonic">Mnemonic phrase</option>
+            <option value="create-wallet">Create new wallet</option>
+          </select>
+        </label>
+
+        {walletType === "create-wallet" ? (
+          <>
+            <br />
+            <button onClick={() => navigate("/create-wallet")}>
+              Create new wallet
+            </button>
+          </>
+        ) : null}
+        <br />
+        {walletType === "encrypted-JSON" ? (
+          <>
+            <label>
+              Upload JSON keystore: <input type="file" />
+            </label>
+            <br />
+          </>
+        ) : (
+          <>
+            <label>
+              Enter your mnemonic phrase:{" "}
+              <input
+                onChange={(e) => setMnemonic(e.target.value)}
+                type="password"
+                value={mnemonic}
+                placeholder="mnemonic phrase..."
+              />
+            </label>
+            <br />
+          </>
+        )}
+
+        <label>
+          Your address:{" "}
+          <input
+            onChange={(e) => setAddressFrom(e.target.value)}
+            type="text"
+            value={addressFrom}
+            placeholder="your address..."
+          />
         </label>
         <br />
         <label>
-          Enter address:
+          Recipient address:{" "}
           <input
             onChange={(e) => setAddressTo(e.target.value)}
             type="text"
             value={addressTo}
-            placeholder="enter address..."
+            placeholder="recipient address..."
           />
         </label>
         <br />
         <label>
-          Enter amount:{" "}
+          Enter amount in Ether:{" "}
           <input
             onChange={(e) => setAmount(e.target.value)}
-            type="text"
+            type="number"
             value={amount}
-            placeholder="enter amount"
+            placeholder="amount in Ether"
           />
-          <select onChange={(e) => setUnits(e.target.value)}>
-            <option value="Ether">Ether</option>
-            <option value="Gwei" type="counter">
-              Gwei
-            </option>
-          </select>
         </label>
         <br />
         <label>
           Gas amount (Gwei):{" "}
           <input
             onChange={(e) => setGas(e.target.value)}
-            type="text"
+            type="number"
             value={gas}
-            placeholder="enter gas in Gwei"
+            placeholder="gas in Gwei"
           />
         </label>
+        <br />
+        <input
+          className="button-54"
+          onSubmit={signTransaction}
+          type="submit"
+          value="Send Transaction"
+        />
       </form>
     </div>
   );
