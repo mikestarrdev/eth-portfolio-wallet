be080e3f5c5da5e3b9420ebd8d82ffdf1ecc9983
Mike Starr <imhs@protonmail.com>
2022-05-17 13:20:49 -0700

    Working on uniswap v3 swapRouterAddress interface with Ethers.js

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -3,13 +3,6 @@ import styled from "styled-components";
 import { useState } from "react";
 import { ethers } from "ethers";
 require("dotenv").config();
-const {
-  abi: IUniswapV3PoolABI,
-} = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
-const {
-  abi: SwapRouterABI,
-} = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json");
-const ERC20ABI = require("./abi.json");
 
 function Swap({ provider, signer }) {
   const REACT_APP_INFURA_KEY = process.env.REACT_APP_INFURA_KEY;
@@ -17,8 +10,8 @@ function Swap({ provider, signer }) {
     `https://rinkeby.infura.io/v3/${REACT_APP_INFURA_KEY}`
   );
   const swapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
-  const swapABI = [
-    "function getPool() private view returns(IUniswapV3Pool)",
+  const ABI = [
+    "function getPool() view returns(IUniswapV3Pool)",
     "function exactInputSingle() external payable returns(uint256)",
     "function exactInput() external payable returns(uint256)",
     "function exactOutputInternal() returns(uint256)",
@@ -26,16 +19,26 @@ function Swap({ provider, signer }) {
     "function exactOutput() external payable returns(uint256)",
   ];
   // DAI/ETH pool
-  const UniEthPool = "0x1d42064Fc4Beb5F8aAF85F4617AE8b3b5B8Bd801";
-  const name0 = "Uniswap";
-  const symbol1 = "UNI";
-  const decimals1 = 18;
+  const poolAddress = "0x1d42064Fc4Beb5F8aAF85F4617AE8b3b5B8Bd801";
 
-  const swapContract = new ethers.Contract(
-    swapRouterAddress,
-    swapABI,
-    provider
-  );
+  const name0 = "Uniswap";
+  const symbol0 = "UNI";
+  const decimals0 = 18;
+  const address0 = "0x986ff0E7487a4B6C6Ba458b84eF261c3c4B62077";
+
+  const name1 = "Wrapped Ether";
+  const symbol1 = "WETH";
+  const decimals1 = 18;
+  const address1 = "0xc778417E063141139Fce010982780140Aa0cD5Ab";
+
+  const swapContract = new ethers.Contract(swapRouterAddress, ABI, provider);
+
+  const main = async () => {
+    const poolContract = await new ethers.Contract(poolAddress, ABI, PROVIDER);
+    console.log(poolContract);
+  };
+
+  main();
 
   return (
     <div className="swap">
